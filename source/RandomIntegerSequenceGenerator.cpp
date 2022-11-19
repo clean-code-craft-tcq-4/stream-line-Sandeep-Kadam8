7064357733a5e53cc30ad44c9ba8edd3c36e5faa
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-11-19 14:33:24 +0530

    change

--- source/RandomIntegerSequenceGenerator.cpp
+++ source/RandomIntegerSequenceGenerator.cpp
@@ -3,17 +3,17 @@
 
 namespace StreamLineSender {
 
-    RandomeIntegerSequenceGenerator::Generate(std::vector<int> &list, int RangeBegin, int RangeEnd, int count) {
+    RandomIntegerSequenceGenerator::Generate(std::vector<int> &list, int RangeBegin, int RangeEnd, int count) {
         auto offset = RangeEnd - RangeBegin;
         list.resize(count);
-        const auto &getRandomNumber = std::bind(&RandomeIntegerSequenceGenerator::GetRandomNumber,
+        const auto &getRandomNumber = std::bind(&RandomIntegerSequenceGenerator::GetRandomNumber,
                                                 this,
                                                 RangeBegin,
                                                 offset);
         std::generate(list.begin(), list.end(), getRandomNumber);
     }
 
-    int GetRandomNumber(int RangeBegin, int offset) {        
+    int RandomIntegerSequenceGenerator::GetRandomNumber(int RangeBegin, int offset) {        
         srand((unsigned) time(NULL));
         return offset + (rand() % RangeBegin);
     }
