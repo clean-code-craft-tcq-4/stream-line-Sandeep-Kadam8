37fcc06d7bc5ef76e9b3781b1c9a30bc1c1e911f
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-11-19 14:21:42 +0530

    change

--- test/TestRandomeIntegerSequenceGenerator.cpp
+++ test/TestRandomeIntegerSequenceGenerator.cpp
@@ -9,4 +9,18 @@ namespace StreamLineSender {
         generator.Generate(list, 10, 20, 5);
         REQUIRE(100 == list.size());
     }
+
+    bool IsValueInRange(7int start, int end, int value) {
+        return (value >= start && value <= end);
+    }
+
+    TEST_CASE("Verify numbers generated are in range") {
+        RandomeIntegerSequenceGenerator generator;
+        std::vector<int> list;
+        int size = 100;
+        generator.Generate(list, 10, 20, 5);
+        for(auto value:list) {
+           REQUIRE(true == IsValueInRange(10, 20, value));
+        }
+    }
 }
