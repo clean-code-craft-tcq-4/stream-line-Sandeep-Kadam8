35d6293a17eeaf17340533b30e44c0fb762cc4da
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-11-19 12:03:34 +0530

    change

--- test/TestSensorParameter.cpp
+++ test/TestSensorParameter.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "SensorParameterList.h"
+#include "../source/SensorParameterList.h"
 
 TEST_CASE("create a sensor parameter object") {
     SensorParameter<int> p("temp", "celsius");
