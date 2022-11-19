8cf3dc1641742b2deea2c20ca58bed0704aec2f8
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-11-19 14:15:57 +0530

    change

--- test/TestRandomeIntegerSequenceGenerator.cpp
+++ test/TestRandomeIntegerSequenceGenerator.cpp
@@ -5,7 +5,7 @@ namespace StreamLineSender {
     TEST_CASE("Generate random integer numbers check size") {
         RandomeIntegerSequenceGenerator generator;
         std::vector<int> list;
-        size = 100;
+        int size = 100;
         generator.Generate(list, 10, 20, 5);
         REQUIRE(100 == list.size());
     }
