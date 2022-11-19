f13b9b4ded2c865e4c6d053a03e89f82035d8c9d
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-11-19 15:26:12 +0530

    cahnge

--- test/TestRandomIntegerSequenceGenerator.cpp
+++ test/TestRandomIntegerSequenceGenerator.cpp
@@ -1,3 +1,5 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "../source/RandomIntegerSequenceGenerator.h"
 #include <vector>
 
@@ -6,22 +8,22 @@ namespace StreamLineSender {
     TEST_CASE("Generate random integer numbers check size") {
         RandomIntegerSequenceGenerator generator;
         std::vector<int> list;
-        int size = 100;
-    //     generator.Generate(list, 10, 20, 5);
-        REQUIRE(100 == list.size());
+        size_t size = 100;
+        generator.Generate(list, 10, 20, size);
+        REQUIRE(size == list.size());
     }
 
     bool IsValueInRange(int start, int end, int value) {
         return (value >= start && value <= end);
     }
 
-    // TEST_CASE("Verify numbers generated are in range") {
-    //     RandomIntegerSequenceGenerator generator;
-    //     std::vector<int> list;
-    //     int size = 100;
-    //     generator.Generate(list, 10, 20, 5);
-    //     for(auto value:list) {
-    //        REQUIRE(true == IsValueInRange(10, 20, value));
-    //     }
-    // }
+    TEST_CASE("Verify numbers generated are in range") {
+        RandomIntegerSequenceGenerator generator;
+        std::vector<int> list;
+        size_t size = 100;
+        generator.Generate(list, 10, 20, size);
+        for(auto value:list) {
+           REQUIRE(true == IsValueInRange(10, 20, value));
+        }
+    }
 }
